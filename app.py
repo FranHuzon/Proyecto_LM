509f1182459cb15b1db4bfa80dfe117853e3992c
Fran Huzón <huzonvillarfranciscojesus@gmail.com>
2018-05-21 09:40:09 +0200

    Modificaciones en el fichero app.py

--- app.py
+++ app.py
@@ -33,99 +33,62 @@ token_url = "https://accounts.google.com/o/oauth2/token"
 
 @app.route('/google')
 def google():
-    return render_template("oauth2.html")
+	return render_template("oauth2.html")
  
 def token_valido():
-    try:
-        token=json.loads(session["token"])
-    except:
-        token = False
-    if token:
-        token_ok = True
-        try:
-            oauth2 = OAuth2Session(os.environ["client_id"], token=token)
-            r = oauth2.get('https://www.googleapis.com/oauth2/v1/userinfo')
-        except TokenExpiredError as e:
-            token_ok = False
-    else:
-        token_ok = False
-    return token_ok
+	try:
+		token=json.loads(session["token"])
+	except:
+		token = False
+	if token:
+		token_ok = True
+		try:
+			oauth2 = OAuth2Session(os.environ["client_id"], token=token)
+			r = oauth2.get('https://www.googleapis.com/oauth2/v1/userinfo')
+		except TokenExpiredError as e:
+			token_ok = False
+	else:
+		token_ok = False
+	return token_ok
 
 @app.route('/perfil')
 def info_perfil():
   if token_valido():
-    return redirect("/perfil_usuario")
+	return redirect("/perfil_usuario")
   else:
-    oauth2 = OAuth2Session(os.environ["client_id"], redirect_uri=redirect_uri,scope=scope)
-    authorization_url, state = oauth2.authorization_url('https://accounts.google.com/o/oauth2/auth')
-    session.pop("token",None)
-    session["oauth_state"]=state
-    return redirect(authorization_url)  
+	oauth2 = OAuth2Session(os.environ["client_id"], redirect_uri=redirect_uri,scope=scope)
+	authorization_url, state = oauth2.authorization_url('https://accounts.google.com/o/oauth2/auth')
+	session.pop("token",None)
+	session["oauth_state"]=state
+	return redirect(authorization_url)  
 
 @app.route('/google_callback')
 def get_token():
-    oauth2 = OAuth2Session(os.environ["client_id"], state=session["oauth_state"],redirect_uri=redirect_uri)
-    token = oauth2.fetch_token(token_url, client_secret=os.environ["client_secret"],authorization_response=request.url[:4]+"s"+request.url[4:])
-    session["token"]=json.dumps(token)
-    return redirect("/perfil_usuario")
+	oauth2 = OAuth2Session(os.environ["client_id"], state=session["oauth_state"],redirect_uri=redirect_uri)
+	token = oauth2.fetch_token(token_url, client_secret=os.environ["client_secret"],authorization_response=request.url[:4]+"s"+request.url[4:])
+	session["token"]=json.dumps(token)
+	return redirect("/perfil_usuario")
 
 @app.route('/perfil_usuario')
 def info_perfil_usuario():
-    if token_valido():
-        token=json.loads(session["token"])
-        oauth2 = OAuth2Session(os.environ["client_id"], token=token)
-        r = oauth2.get('https://www.googleapis.com/oauth2/v1/userinfo')
-        doc=json.loads(r.content)
-        return render_template("perfil.html", datos=doc)
-    else:
-        return redirect('/perfil')
+	if token_valido():
+		token=json.loads(session["token"])
+		oauth2 = OAuth2Session(os.environ["client_id"], token=token)
+		r = oauth2.get('https://www.googleapis.com/oauth2/v1/userinfo')
+		doc=json.loads(r.content)
+		return render_template("perfil.html", datos=doc)
+	else:
+		return redirect('/perfil')
 
 @app.route('/logout')
 def salir():
-    session.pop("token",None)
-    return redirect("/perfil")
-
-
-
+	session.pop("token",None)
+	return redirect("/perfil")
 
 
 
 
 
 if __name__ == '__main__':
-    port=os.environ["PORT"]
+	port=os.environ["PORT"]
 	app.run('0.0.0.0',int(port), debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
