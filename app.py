9685dc70a363a9f942ea7a5806fe12c17bcdd17d
Fran Huzón <huzonvillarfranciscojesus@gmail.com>
2018-06-04 20:10:56 +0200

    Modificación en el fichero app.py

--- app.py
+++ app.py
@@ -29,9 +29,9 @@ def buscar():
 			lista.append(i)
 		return render_template('mostrar.html',l=lista)
 
-@app.route('/detalles',methods=['GET', 'POST'])
-def detalles(id):
-	url="https://www.googleapis.com/books/v1/volumes/"+id
+@app.route('/detalles/<string:id_libro>',methods=['GET', 'POST'])
+def detalles(id_libro):
+	url="https://www.googleapis.com/books/v1/volumes/"+id_libro
 	campos='saleInfo(buyLink,country,isEbook,listPrice),volumeInfo(authors,averageRating,categories,description,imageLinks/small,previewLink,ratingsCount,subtitle,title)'
 	payload={'fields':campos}
 	r=requests.get(url,params=payload)
