06c201ca5e7dd3792667f31ea0a51b821f86974a
Fran Huzón <huzonvillarfranciscojesus@gmail.com>
2018-06-04 11:56:01 +0200

    Modificado app.py

--- app.py
+++ app.py
@@ -110,8 +110,6 @@ def coleccion():
 	else:
 		return redirect('/entrar')
 
-app.run('0.0.0.0',8080,debug=True)
-
-#if __name__ == '__main__':
-#	port=os.environ["PORT"]    
-#	app.run('0.0.0.0',int(port),debug=True)
+if __name__ == '__main__':
+	port=os.environ["PORT"]    
+	app.run('0.0.0.0',int(port),debug=True)
